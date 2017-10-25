26c2e20c8df87895e0795301ad4939c12a55054e
Arthur Clemens <arthurclemens@gmail.com>
2017-10-25 11:02:18 +0200

    Polythene for React setup example

---
+++ README.md
@@ -0,0 +1,27 @@
+# Setup of Polythene 1.0 for React
+
+Compatible with React 16.x
+
+Minimal setup example how to create a [Polythene](https://github.com/ArthurClemens/polythene) app for React.
+
+
+## Setup
+
+init:
+
+~~~
+yarn
+~~~
+
+start development server (port 3000):
+
+~~~
+yarn run dev
+~~~
+
+build:
+
+~~~
+yarn run build
+~~~
+
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { RaisedButton, Dialog } from "polythene-react";
+import { addFastClick } from "polythene-fastclick"; // optional
+
+import "polythene-css/dist/polythene.css";            // Component CSS
+import "polythene-css/dist/polythene-typography.css"; // Default Material Design styles including Roboto font
+
+const App = () => (
+  <div>
+    <RaisedButton
+      label="Show dialog"
+      events={{
+        onClick: () => Dialog.show({
+          /* note the Dialog component is below the other elements in the app */
+          title: "Hello",
+          body: "Click outside to close, or press ESCAPE",
+          backdrop: true
+        })
+      }}
+    />
+    <Dialog />
+  </div>
+);
+
+const mountNode = document.querySelector("#app");
+ReactDOM.render(<App />, mountNode);
